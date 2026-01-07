01306c46d61bbd70de32e6bb72284a9cbcb5d8c2
7qykdn7cff-arch <7qykdn7cff@privaterelay.appleid.com>
2026-01-07 01:13:15 -0500

    Delete build.sh

--- build.sh
+++
@@ -1,3 +0,0 @@
-func -SPA -o storage-contract-code.fif storage_contract.func
-echo "\"storage-contract-code.fif\" include boc>B \"storage-contract-code.boc\" B>file" | fift
-func -SPA -o storage-contract-fabric.fif main.func
